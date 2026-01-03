43e94bffc727a4d2cba1e3cf5767e435839269d8
Enzo Victoriano <ev_leodriguez@hotmail.com>
2026-01-03 13:15:58 -0300

    .

--- Gerar_Pdf.py
+++ Gerar_Pdf.py
@@ -3,7 +3,7 @@ from weasyprint import HTML, CSS
 # Caminhos absolutos (altere se estiver em outra pasta)
 html_path = r"C:\Users\Victoriano\Documents\Projetos em geral\Organização-FrontEnd\Curriculo online\Index.html"
 css_path = r"C:\Users\Victoriano\Documents\Projetos em geral\Organização-FrontEnd\Curriculo online\style.css"
-output_pdf = r"C:\Users\Victoriano\Documents\Projetos em geral\Organização-FrontEnd\Curriculo online\EnzoVictorianoLeodriguez.pdf"
+output_pdf = r"C:\Users\Victoriano\Documents\EnzoVictorianoLeodriguez.pdf"
 
 # Gera o PDF fiel ao layout do navegador
 HTML(html_path).write_pdf(
