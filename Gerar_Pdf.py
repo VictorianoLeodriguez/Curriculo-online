681bb087815131a81c3ce68a1d96e4cda2193566
Enzo Victoriano <ev_leodriguez@hotmail.com>
2026-01-03 12:46:33 -0300

    Atualização de Curriculo

--- Gerar_Pdf.py
+++ Gerar_Pdf.py
@@ -1,9 +1,9 @@
 from weasyprint import HTML, CSS
 
 # Caminhos absolutos (altere se estiver em outra pasta)
-html_path = r"C:\Users\Victoriano\Desktop\Curriculo online\Index.html"
-css_path = r"C:\Users\Victoriano\Desktop\Curriculo online\style.css"
-output_pdf = r"C:\Users\Victoriano\Desktop\Curriculo online\EnzoVictorianoLeodriguez.pdf"
+html_path = r"C:\Users\Victoriano\Documents\Projetos em geral\Organização-FrontEnd\Curriculo online\Index.html"
+css_path = r"C:\Users\Victoriano\Documents\Projetos em geral\Organização-FrontEnd\Curriculo online\style.css"
+output_pdf = r"C:\Users\Victoriano\Documents\Projetos em geral\Organização-FrontEnd\Curriculo online\EnzoVictorianoLeodriguez.pdf"
 
 # Gera o PDF fiel ao layout do navegador
 HTML(html_path).write_pdf(
